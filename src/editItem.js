61c4894274e203acfd8999bcb4d0bb2d69dd7f3f
Tumworobere Annah <annaht.annah@gmail.com>
2021-11-08 18:34:05 +0300

    Delete src directory

--- src/editItem.js
+++
@@ -1,8 +0,0 @@
-import { saveInStorage } from './storage.js';
-
-const editItem = (list, task, textHolder) => {
-  task.description = textHolder.textContent;
-  saveInStorage(list);
-};
-
-export default editItem;
--- src/status.js
+++
@@ -1,7 +0,0 @@
-import { saveInStorage } from './storage.js';
-
-const status = (task, list) => {
-  task.completed = !task.completed;
-  saveInStorage(list);
-};
-export default status;
