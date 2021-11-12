9f6dcc67ccf8e6df6bf47039c182789abf38e317
Tumworobere <annaht.annah@gmail.com>
2021-11-12 16:10:20 +0300

    Clear all completed Items

--- src/test/clearAll.test.js
+++ src/test/clearAll.test.js
@@ -2,8 +2,8 @@ import clearAll from '../clearAll.js';
 
 jest.mock('../clearAll');
 
-describe('Clear all completed function', () => {
-  test('Remove all completed items from list', () => {
+describe('Clear all completed function',() => {
+  test('Remove all items that are completed from list',() => {
     expect(clearAll().length).toBe(1);
   });
 });
