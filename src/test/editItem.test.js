6c5fc2045ab6b362d023d1215627f3176c95164f
Tumworobere <annaht.annah@gmail.com>
2021-11-12 15:43:52 +0300

    Edit task description

--- src/test/editItem.test.js
+++ src/test/editItem.test.js
@@ -2,8 +2,8 @@ import editItem from '../editItem.js';
 
 jest.mock('../editItem');
 
-describe('Edit function', () => {
-  test('Edit a list item', () => {
-    expect(editItem()).toBe('some state...');
+describe('Edit function',() => {
+  test('Edit a list item',() => {
+    expect(editItem()).toBe('some state....');
   });
 });
