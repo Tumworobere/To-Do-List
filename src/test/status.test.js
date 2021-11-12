8bbd2c6a3c551def0ebd25ec2d7ccae66f0bc421
Tumworobere <annaht.annah@gmail.com>
2021-11-12 16:23:27 +0300

    Update Status

--- src/test/status.test.js
+++ src/test/status.test.js
@@ -2,8 +2,8 @@ import status from '../status.js';
 
 jest.mock('../status');
 
-describe('Status change function', () => {
-  test('Check list item when status changes', () => {
+describe('Status change function',() => {
+  test('Check list item when status changes',() => {
     expect(status()).toBe(true);
   });
 });
