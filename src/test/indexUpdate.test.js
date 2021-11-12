237670b2d82c2782a5a215922c21800732588728
Tumworobere <annaht.annah@gmail.com>
2021-11-12 16:04:34 +0300

    Update items with status completed

--- src/test/indexUpdate.test.js
+++ src/test/indexUpdate.test.js
@@ -2,8 +2,8 @@ import indexUpdate from '../indexUpdate.js';
 
 jest.mock('../indexUpdate');
 
-describe('Index update function', () => {
-  test('Update the index of list when one item is deleted', () => {
+describe('Index update function',() => {
+  test('Update the index of list when one item is deleted',() => {
     expect(indexUpdate()).toEqual([1, 2]);
   });
 });
